f31a78a1cd4a4449c30a7c96dd054db44abaefe2
Sachin Supunthaka <sachinsupunthaka96@gmail.com>
2021-08-27 06:55:45 +0530

    chore:addwebviefor the project

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,8 @@ import 'package:provider/provider.dart';
 
 import './screens/bottom_nav_screen.dart';
 
-void main() {
-  runApp(MyApp());
+ main(){
+   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,7 +17,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => CovidData([]),
-      
       child: MaterialApp(
         title: 'Covid-19 CareApp',
         debugShowCheckedModeBanner: false,
@@ -28,10 +27,10 @@ class MyApp extends StatelessWidget {
         ),
         home: BottomNavScreen(),
         routes: {
-          StatusScreen.routeName : (context) => StatusScreen(),
+          StatusScreen.routeName: (context) => StatusScreen(),
           MapScreen.routeName: (context) => MapScreen(),
           HomeScreen.routeName: (context) => HomeScreen(),
-          InformationScreen.routeName : (context) => InformationScreen()
+          InformationScreen.routeName: (context) => InformationScreen()
         },
       ),
     );
--- lib/screens/information.dart
+++ lib/screens/information.dart
@@ -1,24 +1,63 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 import '../widgets/custom_app_bar.dart';
 
-class InformationScreen extends StatelessWidget {
+class InformationScreen extends StatefulWidget {
   static const routeName = '/informationScreen';
-  // const InformationScreen({Key? key}) : super(key: key);
 
-  final List<dynamic> dummy = [];
+  @override
+  _InformationScreenState createState() => _InformationScreenState();
+}
+
+class _InformationScreenState extends State<InformationScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    const url = "https://vocaroo.com/";
     return Scaffold(
       appBar: CustomAppBar(),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Hit Me'),
-          onPressed: () {
-            print("button pressed");
-          },
-        ),
+      body: 
+      // Container(
+      //   child: InAppWebView(
+      //     initialUrlRequest: URLRequest(url: Uri.parse(url)),
+      //     initialOptions: InAppWebViewGroupOptions(
+      //       crossPlatform: InAppWebViewOptions(
+      //         mediaPlaybackRequiresUserGesture: false,
+      //       ),
+      //       android: AndroidInAppWebViewOptions(
+      //         useHybridComposition: true,
+      //       ),
+      //     ),
+      //     androidOnPermissionRequest: (InAppWebViewController controller,
+      //         String origin, List<String> resources) async {
+      //       return PermissionRequestResponse(
+      //           resources: resources,
+      //           action: PermissionRequestResponseAction.GRANT);
+      //     },
+      //   ),
+      // ),
+      WebView(
+        initialUrl: url,
+        javascriptMode: JavascriptMode.unrestricted,
+        onProgress: (int progress) {
+          CircularProgressIndicator(
+            color: Colors.amber,
+          );
+          print('loading :$progress');
+        },
       ),
     );
   }
